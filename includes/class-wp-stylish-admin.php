35b00ce2242442b9a49649f2da8b99811f242c58
tutsocean <anand.deepak9988@gmail.com>
2020-09-14 20:29:23 +0530

    emoved YAGNI files

--- includes/class-wp-stylish-admin.php
+++ includes/class-wp-stylish-admin.php
@@ -77,7 +77,7 @@ class Wp_Stylish_Admin {
 		$this->load_dependencies();
 		$this->set_locale();
 		$this->define_admin_hooks();
-		$this->define_public_hooks();
+		//$this->define_public_hooks();
 
 	}
 
@@ -120,7 +120,7 @@ class Wp_Stylish_Admin {
 		 * The class responsible for defining all actions that occur in the public-facing
 		 * side of the site.
 		 */
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/class-wp-stylish-admin-public.php';
+		//require_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/class-wp-stylish-admin-public.php';
 
 		$this->loader = new Wp_Stylish_Admin_Loader();
 
@@ -166,14 +166,14 @@ class Wp_Stylish_Admin {
 	 * @since    1.0.0
 	 * @access   private
 	 */
-	private function define_public_hooks() {
+	/*private function define_public_hooks() {
 
 		$plugin_public = new Wp_Stylish_Admin_Public( $this->get_plugin_name(), $this->get_version() );
 
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
 
-	}
+	}*/
 
 	/**
 	 * Run the loader to execute all of the hooks with WordPress.
